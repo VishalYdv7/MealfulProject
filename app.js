4c2afc1693bf6e979cf3d55e2edb05d33bc108a4
VishalYdv7 <112755540+VishalYdv7@users.noreply.github.com>
2023-03-10 12:45:15 +0530

    final

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 // const https = require("https");
+const ejs = require("ejs");
 
-const fetchData = require("./logic.js");
-const processData = require("./logic.js");
+const logic = require("./logic.js");
 // const ScheduleItem = require("./ScheduleItem");
 
 const app = express();
+
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,14 +16,40 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function (req, res) {
-  res.send("<p>Thanks</p>");
-  console.log(req.body.date);
+app.get("/styles.css", function (req, res) {
+  res.sendFile(__dirname + "/styles.css");
 });
 
-app.get("/charts", function (req, res) {
-  res, sendFile(__dirname + "/chart.js");
+app.post("/", async function (req, res) {
+  // res.send("<p>Thanks</p>");
+  // console.log(req.body.date);
+  const data = await logic.fetchData(req.body.date);
+  const time = await logic.fetchTime(req.body.date);
+  const myArray = Array.from(data);
+  const timeArray = Array.from(time);
+  // console.log(data);
+  console.log(JSON.stringify(myArray));
+  let dataElements = [];
+  let valueElements = [];
+
+  for (let i = 0; i < myArray.length; i++) {
+    dataElements.push(myArray[i][0].toString());
+  }
+  for (let i = 0; i < myArray.length; i++) {
+    valueElements.push(myArray[i][1]);
+  }
+  console.log(typeof dataElements[0]);
+  console.log(typeof valueElements[0]);
+  console.log(dataElements);
+  console.log(valueElements);
+  console.log("Start of script");
+  res.render("displayGraph", { myArray, timeArray });
+  console.log("End of script");
 });
+
+// app.get("/charts", function (req, res) {
+//   res, sendFile(__dirname + "/displayGraph.ejs");
+// });
 
 app.listen(3000, function () {
   console.log("Server is up!");
--- logic.js
+++ logic.js
@@ -1,19 +1,26 @@
 const https = require("https");
+const { resolve } = require("path");
 const ScheduleItem = require("./ScheduleItem");
 
-fetchData();
-
-function fetchData() {
+// fetchData();
+function fetchData(date) {
   const url = "https://www.jsonkeeper.com/b/P2VO";
-  https.get(url, function (response) {
-    let data = "";
-    response.on("data", function (chunk) {
-      data += chunk;
-    });
-    response.on("end", () => {
-      const apiResponse = JSON.parse(data);
-      processData(apiResponse, "2021-05-21");
-    });
+
+  return new Promise((resolve, reject) => {
+    try {
+      https.get(url, function (response) {
+        let data = "";
+        response.on("data", function (chunk) {
+          data += chunk;
+        });
+        response.on("end", () => {
+          const apiResponse = JSON.parse(data);
+          resolve(processData(apiResponse, date));
+        });
+      });
+    } catch (error) {
+      reject(error);
+    }
   });
 }
 
@@ -41,7 +48,83 @@ function processData(apiResponse, inputDate) {
     );
     // scheduleList.push(scheduleItem);
     if (inputDate === scheduleItem.item_date) {
-      /// scheduleItem.schedule_time.foreach((dateString) => {
+      const dateString = new Date(scheduleItem.schedule_time);
+      const date = dateString.getDate();
+      const month = dateString.getMonth();
+      const year = dateString.getFullYear();
+      const hour = dateString.getHours();
+      // let timeSlot = "";
+      let count = "";
+      for (const [key, value] of timeSlots.entries()) {
+        if (hour >= (key - 1) * 3 && hour < key * 3) {
+          timeSlot = value;
+          count++;
+          break;
+        }
+      }
+      if (outputMap.has(date + "-" + month + "-" + year)) {
+        outputMap.set(
+          date + "-" + month + "-" + year,
+          outputMap.get(date + "-" + month + "-" + year) + 1
+        );
+      } else {
+        outputMap.set(date + "-" + month + "-" + year, count);
+      }
+    }
+  }
+  // for (const [date, count] of outputMap) {
+  //   console.log(`${date}: ${count}`);
+  // }
+  return outputMap;
+}
+
+// fetchTime();
+
+function fetchTime(date) {
+  const url = "https://www.jsonkeeper.com/b/P2VO";
+
+  return new Promise((resolve, reject) => {
+    try {
+      https.get(url, function (response) {
+        let data = "";
+        response.on("data", function (chunk) {
+          data += chunk;
+        });
+        response.on("end", () => {
+          const apiResponse = JSON.parse(data);
+          resolve(processTime(apiResponse, date));
+        });
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function processTime(apiResponse, inputDate) {
+  console.log("processing data...........");
+  const scheduleList = [];
+  const outputMap = new Map();
+
+  const timeSlots = new Map([
+    [1, "12am-3am"],
+    [2, "3am-6am"],
+    [3, "6am-9am"],
+    [4, "9am-12pm"],
+    [5, "12pm-3pm"],
+    [6, "3pm-6pm"],
+    [7, "6pm-9pm"],
+    [8, "9pm-12am"],
+  ]);
+
+  for (const item of apiResponse) {
+    const scheduleItem = new ScheduleItem(
+      item.schedule_time,
+      item.slot,
+      item.item_date
+    );
+    // scheduleList.push(scheduleItem);
+    if (inputDate === scheduleItem.item_date) {
       const dateString = new Date(scheduleItem.schedule_time);
       const date = dateString.getDate();
       const month = dateString.getMonth();
@@ -56,17 +139,23 @@ function processData(apiResponse, inputDate) {
           break;
         }
       }
-      if (outputMap.has(date + " " + month + " " + year + " " + timeSlot)) {
+      if (outputMap.has(date + "-" + month + "-" + year + "-" + timeSlot)) {
         outputMap.set(
-          date + " " + month + " " + year + " " + timeSlot,
-          outputMap.get(date + " " + month + " " + year + " " + timeSlot) + 1
+          date + "-" + month + "-" + year + "-" + timeSlot,
+          outputMap.get(date + "-" + month + "-" + year + "-" + timeSlot) + 1
         );
       } else {
-        outputMap.set(date + " " + month + " " + year + " " + timeSlot, count);
+        outputMap.set(date + "-" + month + "-" + year + "-" + timeSlot, count);
       }
     }
   }
-  for (const [date, count] of outputMap) {
-    console.log(`${date}: ${count}`);
-  }
+  // for (const [date, count] of outputMap) {
+  //   console.log(`${date}: ${count}`);
+  // }
+  return outputMap;
 }
+
+module.exports = {
+  fetchData: fetchData,
+  fetchTime: fetchTime,
+};
